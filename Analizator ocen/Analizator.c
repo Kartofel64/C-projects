6d8b3ff42b06b9ddaabc55c34175cfdc177f3773
Wiktor Twardowski <w.twardowski.0@gmail.com>
2026-09-19 20:51:57 +0200

    add_grade function

--- Analizator ocen/Analizator.c
+++ Analizator ocen/Analizator.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+
+int grades[10];
+int grades_count;
+
+void add_grade(){
+    printf("Ile ocen chcesz podac? ");
+    scanf("%d", &grades_count);
+
+    for (int i = 0; i < grades_count; i++) {
+        printf("Podaj ocene: ");
+        scanf("%d", &grades[i]);
+    }
+};
 
 int main(){
 
@@ -15,11 +29,21 @@ int main(){
 };
 
     int length = sizeof(menu) / sizeof(menu[0]);
-    printf("%d\n", length);
+    int choice = 0;
     while(!exit){
         //printf("===== ANALIZATOR OCEN =====\n");
         for(int i = 0; i < length; i++){
             printf("%d. %s\n", i + 1, menu[i]);
+        }
+        printf("Wybierz opcje: ");
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 1:
+                add_grade();
+                break;
+            case 7:
+                exit = true;
         }
     }
     return 0;
