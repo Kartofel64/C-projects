09c6d88f1fd7e073df06e8628794acefd551755c
Wiktor Twardowski <w.twardowski.0@gmail.com>
2026-09-25 13:02:16 +0200

    Add matrix functionality with input handling and statistics calculations

--- Statystyki tablicy/Statystyki.c
+++ Statystyki tablicy/Statystyki.c
@@ -1,6 +1,101 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void matrix(){
+    int row = 0, col = 0;
+
+    printf("\n\nPodaj liczbe w poziomie: ");
+    scanf("%d", &row);
+
+    printf("\n\nPodaj liczbe w pionie: ");
+    scanf("%d", &col);
+
+    if (row <= 0 || col <= 0){
+        printf("\n\nLiczba musi byc dodatnia");
+    }else{
+        int matrix_array[row][col];
+
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                printf("\n\nPodaj element [%d][%d]: \n", i+1, j+1);
+                scanf("%d", &matrix_array[i][j]);
+            }
+        }
+
+        printf("\n\n");
+
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                printf("%d ", matrix_array[i][j]);
+            }
+            printf("\n");
+        }
+
+        int sum = 0, max = matrix_array[0][0], sumR = 0;
+
+        int collsum[col];
+        int rowsum[row];
+
+        for (int j = 0; j < col; j++) {
+            collsum[j] = 0;
+        }
+
+        for (int i = 0; i < row; i++) {
+            sumR = 0;
+
+            for (int j = 0; j < col; j++) {
+                sum += matrix_array[i][j];
+
+                if (max < matrix_array[i][j]) {
+                    max = matrix_array[i][j];
+                }
+
+                sumR += matrix_array[i][j];
+                collsum[j] += matrix_array[i][j];
+            }
+
+            rowsum[i] = sumR;
+        }
+
+
+        printf("\n\nSuma wszystkich elementow: %d", sum);
+        printf("\nNajwiekszy element: %d", max);
+        printf("\nSuma wierszy: ");
+        for (int i = 0; i < row; i++){
+            printf("%d ", rowsum[i]);
+        }
+        printf("\nSuma kolumn: ");
+        for (int i = 0; i < col; i++){
+            printf("%d ", collsum[i]);
+        }
+    }
+}
+
+void search(int sorted_array[], int array_elements){
+    int number = 0;
+    printf("\n\nPodaj liczbe do wyszukania: ");
+    scanf("%d", &number);
+
+    int index[array_elements];
+
+    for (int i = 0; i < array_elements; i++){
+        index[i] = false;
+    }
+
+    for (int i = 0; i < array_elements; i++){
+        if (sorted_array[i] == number){
+            index[i] = true;
+        }
+    }
+
+    printf("\n\nLiczba jest na pozycji: ");
+    for (int i = 0; i < array_elements; i++){
+        if (index[i] == true){
+            printf("%d ", i+1);
+        }
+    }
+}
+
 void sort(int array[], int array_elements){
     printf("\n\nTablica przed sortowaniem: ");
     for (int i = 0; i < array_elements; i++){
@@ -27,13 +122,15 @@ void sort(int array[], int array_elements){
     for (int i = 0; i < array_elements; i++){
             printf("%d ", sorted_array[i]);
         }
+
+    search(sorted_array, array_elements);
 }
 
 void array_stats(){
     int array_elements;
     printf("\nPodaj liczbe elementow tablicy: ");
     scanf("%d", &array_elements);
-    if (array_elements < 0){
+    if (array_elements <= 0){
         printf("Liczba nie może być ujemna-!\n");
     }else{
         int array[array_elements];
@@ -102,6 +199,7 @@ int main(){
             }
             
             case 2: {
+                matrix();
                 break;
             }
 
